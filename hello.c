4b0f7d66c275e3f25bc7d2dbd17914541cb018cf
mylak005 <64530452+mylak005@users.noreply.github.com>
2020-07-06 23:42:10 +0200

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdio>
+
+int main()
+{
+   cout << "Hello World" << endl;
+   return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,5 @@
+#include <iostream.h>
+
+int main(void) {
+  printf("Hello World");
+}
